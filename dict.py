0fc1b5f979604248af1c205097584d7a8b44c7f3
Chinmayi Kallurmath <chinmayikallurmath@gmail.com>
2025-03-29 13:17:30 +0530

    Add files via upload

--- dict.py
+++ dict.py
@@ -1,12 +1,28 @@
 
 
-bid_mem = {
-    "dimpu": "350",
-    "sam" : "200"
-}
+def find_highest_bid(bid_dictionary):
+    winner = ""
+    highest_bid = 0
+    for bidder in bid_dictionary:
+        bid_amt = bid_dictionary[bidder]
+        if bid_amt > highest_bid:
+            highest_bid = bid_amt
+            winner = bidder
 
-name = str(input("Enter your name: "))
-bid_amt = int(input("Enter your bid amount: "))
+    print(f"The winner is {winner}")
 
-bid_mem(key = name, value= bid_amt).append(bid_mem)
-print(bid_mem)
+
+bid = {}
+bid_continue = True
+while bid_continue:
+    name = str(input("Enter your name: \n"))
+    bid_amt = int(input("Enter your bid amount: \n"))
+    bid[name] = bid_amt
+    any_bidder = input("Any other person is there want to bid? y for yes and n for no: \n")
+    if any_bidder == "n":
+        bid_continue = False
+        find_highest_bid(bid)
+    else:
+        print( "\n" * 20)
+
+
