206b8862c80684eea919236a06f7d9b9424adebf
Chinmayi Kallurmath <chinmayikallurmath@gmail.com>
2025-04-09 19:52:43 +0530

    Add files via upload

---
+++ RPS.py
@@ -0,0 +1,4 @@
+print("Welcome to band name generator")
+place = input("type the name of the city you grew up in: ")
+pet = input("type your pet name: ")
+print("your band name is " + place + " " + pet)
